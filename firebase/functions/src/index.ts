19d755fa1243080ea73e8f59343bc0c0350d5b48
Arunkumar <arunk.beece@gmail.com>
2018-04-18 23:51:25 +0530

    Revert temperature logging

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -3,6 +3,10 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
+const MAX_TEMPERATURE_LOG_COUNT = 35
+
+const TEMPERATURE_LOG_PATH = "/pi/health/temperature_log/{pushId}"
+
 export const helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
